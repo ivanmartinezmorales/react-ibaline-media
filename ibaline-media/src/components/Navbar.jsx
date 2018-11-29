483d052c51a35781371872f50c44779bf446fae6
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2018-11-29 13:35:39 -0700

    Working navbar feature

--- ibaline-media/src/components/Navbar.jsx
+++ ibaline-media/src/components/Navbar.jsx
@@ -1,22 +1,69 @@
 import React, { Component } from "react";
 import { Nav, NavItem, NavLink } from "reactstrap";
 
+const NavBarLogo = () => {
+  return <div className="" />;
+};
+
+const NavBarComponentList = () => {
+  /*
+    NavBarComponentsList: Includes all of the unordered list components that are in the navbar
+
+      Focus, Services, Packages, About, Contact
+     */
+  return (
+    <div>
+      <ul className="navbar-nav mr-auto" />
+    </div>
+  );
+};
+
+const NavBarSocialMedia = () => {
+  /*
+  NavBarSocialMedia - Stateless function that returns the social media links to Saloua's social medias
+
+  Social media links required:
+    Instagram -- https://www.instagram.com/salouaibaline
+    Facebook -- https://www.instagram.com/salouaibaline
+    LinkedIn -- https://www.linkedin.com/in/salouaibaline
+
+    With appropriate icons from font-awesome!
+
+   */
+    return(
+        <div>
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item mx-1"><a className="nav-link" href="#">
+                    <i className="fa fa-facebook fa-fw fa-lg"/>
+                </a></li>
+                <li className="nav-item mx-1"><a className="nav-link" href="#">
+                    <i className="fa fa-instagram fa-fw fa-lg"/>
+                </a></li>
+                <li className="nav-item mx-1"><a className="nav-link" href="https://twitter.com/salouaibaline?lang=en">
+                    <i className="fa fa-twitter fa-fw fa-lg"/>
+                </a></li>
+                <li className="nav-item mx-1"><a className="nav-link" href="https://www.linkedin.com/in/salouaibaline/">
+                    <i className="fa fa-linkedin fa-fw fa-lg"/>
+                </a></li>
+            </ul>
+        </div>
+    )
+
+};
+
+const NavBarFrame = () => {
+  return (
+    <div className="nav navbar-expand-lg">
+      <h1>N A V B A R B O I</h1>
+    </div>
+  );
+};
+
 class Navbar extends Component {
   render() {
     return (
       <div>
-        <div id="myNav" class="overlay">
-          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
-            ×
-          </a>
-          <div class="overlay-content">
-            <a href="#">About</a>
-            <a href="#">Services</a>
-            <a href="#">Clients</a>
-            <a href="#">Contact</a>
-          </div>
-        </div>
-        <span onclick="openNav()">open</span>
+        <h1>Placeholder text</h1>
       </div>
     );
   }
